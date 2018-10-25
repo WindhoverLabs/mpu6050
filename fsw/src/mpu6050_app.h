f21d5ae53ceb29e7a09ac1b9848ed2aa367f4f7e
Luke Doman <ldoman@windhoverlabs.com>
2018-10-25 09:02:04 -0500

    Making calibrations commandable

--- fsw/src/mpu6050_app.h
+++ fsw/src/mpu6050_app.h
@@ -460,6 +460,24 @@ public:
      **
      *************************************************************************/
     void UpdateParamsFromTable(void);
+    
+    /************************************************************************/
+    /** \brief Set sensor calibation values
+    **
+    **  \par Description
+    **       Updates the sensor calibration table with passed values.
+    **
+    **  \par Assumptions, External Events, and Notes:
+    **       None
+    **
+    **  \param [in]   CalibrationMsgPtr   A pointer to the calibration messsage
+    **
+    **  \returns
+    **  \retcode #CFE_SUCCESS  \retdesc \copydoc CFE_SUCCESS  \endcode
+    **  \endreturns
+    **
+    *************************************************************************/
+    int32 UpdateCalibrationValues(MPU6050_SetCalibrationCmd_t* CalibrationMsgPtr);
 
 };
 
